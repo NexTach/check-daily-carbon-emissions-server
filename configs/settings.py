c73ead99473e027085230a679f34a7ee1d0229df
송재욱 <haensol976520@gmail.com>
2025-01-25 00:08:08 +0900

    cors

--- configs/settings.py
+++ configs/settings.py
@@ -138,10 +138,31 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    
+    "https://check-daily-carbon-emissions-client-ten.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -157,3 +178,49 @@ REST_FRAMEWORK = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    X_FRAME_OPTIONS = 'DENY'
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'file': {
+                'level': 'ERROR',
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['file'],
+                'level': 'ERROR',
+                'propagate': True,
+            },
+        },
+    }
+
+    ALLOWED_HOSTS = [
+        'localhost',
+        '127.0.0.1',
+        'check-daily-carbon-emissions-client-ten.vercel.app',
+    ]
+
+    CORS_ALLOWED_ORIGINS = [
+        'https://check-daily-carbon-emissions-client-ten.vercel.app',
+    ]
+
+    CSRF_TRUSTED_ORIGINS = [
+        'https://check-daily-carbon-emissions-client-ten.vercel.app',
+    ]
