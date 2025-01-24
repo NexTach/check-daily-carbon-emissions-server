0a4489e133afa59b72fdd421b2d2f25436482a24
송재욱 <haensol976520@gmail.com>
2025-01-25 06:06:19 +0900

    cloudtype setting

--- configs/settings.py
+++ configs/settings.py
@@ -20,10 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*'] 
